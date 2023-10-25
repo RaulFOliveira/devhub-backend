fff1180a8d858bffcbd1cdc1f7805b049724277a
NathanFrancesco03 <nathan.sepulvida@sptech.school>
2023-10-25 17:36:11 -0300

    feat: Corrigindo Cors

---
+++ backend/devhub.api/src/main/java/com/devhub/api/infra/security/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.devhub.api.infra.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Bean
+    public CorsFilter  corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+//        config.applyPermitDefaultValues(); // Aplicar configurações padrão
+//        config.addAllowedOrigin("http://localhost:3000"); // Permitir a origem específica
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true);
+    }
+}
